f43368a2404dff3c729514685bf43303bc6a8e1f
Alex Bennett <alexandergraybennett@gmail.com>
2022-01-01 11:06:41 -0700

    wip: processing microphone audio

--- index.js
+++ index.js
@@ -54,103 +54,79 @@ let frameId;
 // Only start the process when the user clicks the spacebar
 // document.addEventListener("keydown", (key) => {
 
-const player = document.getElementById("audio");
-
-let microphoneMode = true;
+let microphoneMode = false;
 
 var file = document.getElementById("thefile");
 var audio = document.getElementById("audio");
 
 if (microphoneMode) {
   navigator.mediaDevices
-    .getUserMedia({ audio: true, video: false })
+    .getUserMedia({
+      audio: {
+        mandatory: {
+          googEchoCancellation: "true",
+          googAutoGainControl: "true",
+          googNoiseSuppression: "true",
+          googHighpassFilter: "true",
+        },
+        optional: [],
+      },
+      video: false,
+    })
     .then(function (stream) {
       console.log("handling");
       if (window.URL) {
         console.log("srcObject");
-        player.srcObject = stream;
 
         const context = new AudioContext();
-        const src = context.createMediaStreamSource(stream);
-        const analyser = context.createAnalyser();
+        const source = context.createMediaStreamSource(stream);
+        const processor = context.createScriptProcessor(1024, 1, 1);
 
-        src.connect(analyser);
-        analyser.connect(context.destination);
+        source.connect(processor);
+        processor.connect(context.destination);
 
-        analyser.fftSize = 2048;
+        var analyser = context.createAnalyser();
+        analyser.smoothingTimeConstant = 0.2;
+        analyser.fftSize = 128;
 
-        const bufferLength = analyser.frequencyBinCount;
-        console.log(bufferLength);
+        const listener = new THREE.AudioListener();
+        const audio = new THREE.Audio(listener);
 
-        const dataArray = new Uint8Array(bufferLength);
-      } else {
-        console.log("src");
-        player.src = stream;
+        processor.onaudioprocess = function (e) {
+          // Do something with the data, e.g. convert it to WAV
+          // console.log(e.inputBuffer);
+          let spectrum = new Uint8Array(analyser.frequencyBinCount);
+          // getByteFrequencyData returns amplitude for each bin
+          analyser.getByteFrequencyData(spectrum);
+          // getByteTimeDomainData gets volumes over the sample time
+          // analyser.getByteTimeDomainData(self.spectrum);
+          console.log(spectrum);
+        };
+
+        var input = context.createMediaStreamSource(stream);
+        input.connect(analyser);
+        analyser.connect(processor);
+        processor.connect(context.destination);
+
+        //   // ANIMATION LOOP
+
+        //   playing = true;
+        // } else {
+        //   console.log("src");
+        //   player.src = stream;
       }
     })
     .catch((err) => {
       console.error("could not connect to mic");
       console.log(err);
     });
-
-  console.log(audio);
-
-  // if (key.key === " ") {
-  if (audio.paused) {
-    const audioManager = new AudioManager(audio);
-    // AudioManager.play();
-    const analyser = audioManager.analyser();
-
-    // ANIMATION LOOP
-    function animate() {
-      // AUDIO DATA
-      const avgFrequencyData = analyser.getAverageFrequency();
-      const frequencyData = analyser.getFrequencyData();
-
-      // PROCESS AUDIO DATA
-      const { lowerHalfArray, upperHalfArray, lowerAvg, upperAvg } =
-        prepareIcosahedron(frequencyData);
-
-      // run the loop
-      frameId = requestAnimationFrame(animate);
-
-      // adjusts the intensity of the light based on the average frequency
-      coolLight.intensity = avgFrequencyData / 30;
-      warmLight.intensity = avgFrequencyData / 30;
-
-      // Animates the icosahedron
-      animateIcosahedron(avgFrequencyData, frequencyData);
-
-      // Rotates the camera around the scene
-      rotateCameraAroundScene(scene.position, camera);
-
-      // renders the scene
-      renderer.render(scene, camera);
-    }
-
-    // RUN THE ANIMATION LOOP
-    animate();
-
-    playing = true;
-  } else {
-    while (scene.children.length > 0) {
-      scene.remove(scene.children[0]);
-    }
-    cancelAnimationFrame(frameId);
-    playing = false;
-    AudioManager.pause();
-  }
 } else {
   file.onchange = function () {
     var files = this.files;
     audio.src = URL.createObjectURL(files[0]);
 
-    console.log(audio);
-
-    // if (key.key === " ") {
     if (audio.paused) {
       const audioManager = new AudioManager(audio);
-      // AudioManager.play();
       const analyser = audioManager.analyser();
 
       // ANIMATION LOOP
--- scene/audio.js
+++ scene/audio.js
@@ -12,14 +12,40 @@ gui
   });
 
 export class AudioManager {
-  constructor(audio) {
+  constructor(audio, type) {
     this.listener = new THREE.AudioListener();
     this.audio = new THREE.Audio(this.listener);
     this.fftSize = audioConfig.fftSize;
-    this.loader = new THREE.AudioLoader();
-    this.loop = true;
-    this.mediaElement = audio;
-    this.incomingAugio = audio;
+    if (type === "mic") {
+      // this.mediaElement = this.audio;
+      // this.mediaElement.srcObject = this.audio.srcObject;
+      // this.mediaElement.play();
+
+      let stream = audio;
+
+      const tracks = stream.getAudioTracks();
+
+      console.log(tracks);
+
+      console.log("mic!!!!");
+
+      console.log(typeof audio);
+      console.log(audio);
+
+      // const listener = new THREE.AudioListener();
+      // const analyser = new THREE.AudioAnalyser(, this.fftSize);
+      // audioContext.createMediaStreamSource(stream);
+      // const _audio = new THREE.Audio(listener);
+      // const context = listener.context;
+      // const source = context.createMediaElementSource(_audio);
+
+      // audio.setNodeSource(source);
+    } else {
+      this.loader = new THREE.AudioLoader();
+      this.loop = true;
+      this.mediaElement = audio;
+      this.incomingAudio = audio;
+    }
   }
 
   setFFTSize(fftSize) {
@@ -38,22 +64,32 @@ export class AudioManager {
     }
   }
 
-  analyser = () => {
-    if (/(iPad|iPhone|iPod)/g.test(navigator.userAgent)) {
-      this.loader.load(this.incomingAugio, function (buffer) {
-        this.audio.setBuffer(buffer);
-        this.audio.setLoop(this.loop);
-        this.audio.playbackRate(0.2);
-        this.audio.play();
-      });
+  analyser = (type) => {
+    if (!type || type !== "mic") {
+      try {
+        if (/(iPad|iPhone|iPod)/g.test(navigator.userAgent)) {
+          this.loader.load(this.incomingAudio, function (buffer) {
+            this.audio.setBuffer(buffer);
+            this.audio.setLoop(this.loop);
+            this.audio.playbackRate(0.2);
+            this.audio.play();
+          });
+        } else {
+          // this.mediaElement.play();
+          this.audio.setMediaElementSource(this.mediaElement);
+        }
+        const analyser = new THREE.AudioAnalyser(this.audio, this.fftSize);
+
+        return analyser;
+      } catch (e) {
+        console.log(e);
+
+        return "could not make analyser";
+      }
     } else {
-      this.mediaElement.play();
-      this.audio.setMediaElementSource(this.mediaElement);
+      const analyser = new THREE.AudioAnalyser(this.audio, this.fftSize);
+      return analyser;
     }
-
-    const analyser = new THREE.AudioAnalyser(this.audio, this.fftSize);
-
-    return analyser;
   };
 
   voice = () => {
