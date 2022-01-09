b2787196167c104089cdc51022914aea6e0689aa
Alex Bennett <alexandergraybennett@gmail.com>
2022-01-08 17:38:43 -0700

    wip: stable

--- audio.js
+++ audio.js
@@ -1,5 +1,6 @@
 import { start, stop } from "./animation.js";
 
+// Audio element set up
 export function createAudio() {
   // create audio element
   const audio = document.createElement("audio");
@@ -26,20 +27,33 @@ export function createAudio() {
 
 createAudio();
 
+// Audio analyser set up
 function handleAudioAnalzer(files) {
-  // these are global so that they dont get recreated over and over
-  var context = new AudioContext();
-  const _audio = document.getElementById("audio");
-  var src = context.createMediaElementSource(_audio);
-  var analyser = context.createAnalyser();
+  // New Audio Context
+  const context = new AudioContext();
 
+  // Ref to audio element
+  const audio = document.getElementById("audio");
+
+  // Get the audio source from the audio element
+  const src = context.createMediaElementSource(audio);
+
+  // Create the audio analyser from the audio context
+  const analyser = context.createAnalyser();
+
+  // Connect the audio source to the analyser
   src.connect(analyser);
+
+  // Connect the analyser to the audio context destination (speakers)
   analyser.connect(context.destination);
 
+  // Set the fourier size of the analyser
   analyser.fftSize = 2048;
+
   return { analyser, src, context };
 }
 
+// these are global so that they dont get recreated over and over
 const { analyser, src, context } = handleAudioAnalzer();
 
 export { analyser };
--- init.js
+++ init.js
@@ -11,16 +11,16 @@ export function handleOnLoad() {
   };
 }
 
+// when a file is uploaded, connect it to the audio element
+function handleFileChange(files, audio) {
+  audio.src = URL.createObjectURL(files[0]);
+  audio.load();
+}
+
 // get audio and file upload elements
 function getAudioAndFile() {
   const file = document.getElementById("thefile");
   const audio = document.getElementById("audio");
 
   return { file, audio };
-}
-
-// when a file is uploaded, connect it to the audio element
-function handleFileChange(files, audio) {
-  audio.src = URL.createObjectURL(files[0]);
-  audio.load();
 }
