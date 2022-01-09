5e010b6a08c7f8029c137feda22e94b072bb9588
Alex Bennett <alexandergraybennett@gmail.com>
2022-01-08 18:13:29 -0700

    wip: stable

--- animation.js
+++ animation.js
@@ -8,18 +8,24 @@ import { analyser, bufferLength, dataArray } from "./audio.js";
 
 export function renderFrame() {
   animation.id = requestAnimationFrame(renderFrame);
-
   analyser.getByteFrequencyData(dataArray);
 
-  console.log(dataArray);
-
+  // clear the canvas
   ctx.fillStyle = "#000";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+  // x is for the bar offset on X axis
   let x = 0;
-  let barHeight;
-  var barWidth = (canvas.width / bufferLength) * 2.5;
 
+  // placeholder for bar height
+  let barHeight;
+
+  // the width of each bar
+  let barWidth = (canvas.width / bufferLength) * 2.5;
+
+  // for each frequency bin in the array
+  // loop through the dataArray and draw a bar
+  // then increment the x position by the bar width
   for (var i = 0; i < bufferLength; i++) {
     barHeight = dataArray[i];
     paintBar(i, barHeight, bufferLength, x);
@@ -27,6 +33,7 @@ export function renderFrame() {
   }
 }
 
+// paint the bar depending on the freuqency amplitude for the height
 export function paintBar(i, barHeight, bufferLength, x) {
   var WIDTH = canvas.width;
   var HEIGHT = canvas.height;
