b539b3fdc6c0049cc2aa22c0e5f95f780e391844
Alex Bennett <alexandergraybennett@gmail.com>
2022-01-09 11:48:06 -0700

    wip: CTA

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 import styles from "./style.css";
 
-document.body.addEventListener("click", (e) => {
-  console.log("click!");
+// create a new button called get started
+const getStartedButton = document.createElement("button");
+getStartedButton.innerText = "Get Started";
+getStartedButton.className = "getStartedButton";
+getStartedButton.addEventListener("click", handleGetStartedButtonClick);
+// append the button to the DOM
+document.body.appendChild(getStartedButton);
+
+function handleGetStartedButtonClick() {
   import("./new/init.js").then(({ handleOnLoad }) => {
     handleOnLoad();
+
+    // remove the button from the DOM
+    document.body.removeChild(getStartedButton);
   });
-});
+}
+
+// document.body.addEventListener("click", (e) => {
+//   console.log("click!");
+// });
